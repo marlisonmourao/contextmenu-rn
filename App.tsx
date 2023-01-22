61d5da53d386414d63618ca08d45c30593aa6be1
marlisonmourao <bentesmourao@gmail.com>
2023-01-22 17:08:42 -0400

    initial commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import 'react-native-gesture-handler';
+import { HoldMenuProvider } from 'react-native-hold-menu';
+import { Feather } from '@expo/vector-icons'
+
+import { StatusBar } from 'react-native';
+
+import { Home } from './src/screens/Home';
+
+export default function App() {
+  return (
+    <HoldMenuProvider theme="dark" iconComponent={Feather}>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      <Home />
+    </HoldMenuProvider>
+  );
+}
